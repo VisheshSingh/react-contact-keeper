228e3a4d603b4d8bf86a17bcc8faef93f0b4b3a9
VisheshSingh <visheshsingh16@gmail.com>
2019-07-07 14:17:42 -0500

    console warning

--- config/db.js
+++ config/db.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-  mongoose
-    .connect(db, {
-      useURLParser: true,
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false
-    })
-    .then(() => console.log('Connection to MongoDB successful ✌'))
-    .catch(err => {
-      console.log(err.message);
-      process.exit(1);
     });
+
+    console.log('Connection to MongoDB successful ✌');
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
