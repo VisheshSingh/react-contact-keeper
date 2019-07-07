ebc7512b50e7b53120f325e63838f706a6f594e5
VisheshSingh <visheshsingh16@gmail.com>
2019-07-07 14:01:06 -0500

    Backend routes

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to ContactKeeper API 🤞' })
 );
 
+// DEFINE ROUTES
+const auth = require('./routes/auth');
+const contacts = require('./routes/contacts');
+const users = require('./routes/users');
+
+app.use('/api/auth', auth);
+app.use('/api/contacts', contacts);
+app.use('/api/users', users);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
