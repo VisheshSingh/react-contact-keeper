723771a91c49fcf2f936dd82edd0567a071e02cb
VisheshSingh <visheshsingh16@gmail.com>
2019-07-07 16:35:00 -0500

    validation using express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please add name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Password must be minimum 6 characters long').isLength({
+      min: 6
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(404).json({ errors: errors.array() });
+    }
+    res.send('passed 👍');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const app = express();
 
 // Connect Database
 connectDB();
 
-const app = express();
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to ContactKeeper API 🤞' })
 );
