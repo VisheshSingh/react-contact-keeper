ab93abde714fa77ceaffd3f8216c444437deab38
VisheshSingh <visheshsingh16@gmail.com>
2019-07-14 17:04:24 -0500

    Add contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 // Middlware
 const auth = require('../middleware/auth');
 
@@ -9,7 +10,7 @@ const Contacts = require('../models/Contacts');
 
 // @route       GET api/contacts
 // @desc        Get all user contacts
-// @access      Public
+// @access      Private
 router.get('/', auth, async (req, res) => {
   try {
     const contacts = await Contacts.find({ user: req.user.id }).sort({
@@ -24,21 +25,54 @@ router.get('/', auth, async (req, res) => {
 
 // @route       POST api/contacts
 // @desc        Register a user
-// @access      Public
-router.post('/', (req, res) => {
-  res.send('Add contact');
-});
+// @access      Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contacts({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error 🧬');
+    }
+  }
+);
 
 // @route       UPDATE api/contacts/:id
 // @desc        Update contact
-// @access      Public
+// @access      Private
 router.put('/:id', (req, res) => {
   res.send('Update contact');
 });
 
 // @route       DELETE api/contacts/:id
 // @desc        Delete contact
-// @access      Public
+// @access      Private
 router.delete('/:id', (req, res) => {
   res.send('Delete contact');
 });
