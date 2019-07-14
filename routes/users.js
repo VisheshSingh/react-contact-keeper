589f5ae9d00f301e9e428b7eb4bc1a23bf8e8d8a
VisheshSingh <visheshsingh16@gmail.com>
2019-07-14 15:11:56 -0500

    Create & Respond With JSON Web Token

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../models/User');
 
@@ -44,7 +46,23 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save();
 
-      res.send('User saved 📥');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.send({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error 🧬');
