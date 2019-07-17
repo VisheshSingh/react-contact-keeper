dc3e2cc19a352d01424b4bf9ce6e7b48a6c1bf7e
VisheshSingh <visheshsingh16@gmail.com>
2019-07-16 21:26:28 -0500

    About component

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p className="my-1">A full stack React app for storing contacts</p>
+      <p className="bg-dark p">
+        <strong>Version: </strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
