f8934f30d7e72bb9b9eb7347e93dbbbf0926c085
VisheshSingh <visheshsingh16@gmail.com>
2019-07-20 13:10:59 -0500

    react css transitions

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useContext } from 'react';
 import ContactState from '../../context/contact/contactContext';
 import ContactItem from './ContactItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Contacts = () => {
   const contactContext = useContext(ContactState);
@@ -11,13 +12,23 @@ const Contacts = () => {
   }
   return (
     <Fragment>
-      {filtered === null
-        ? contacts.map(contact => {
-            return <ContactItem key={contact.id} contact={contact} />;
-          })
-        : filtered.map(contact => {
-            return <ContactItem key={contact.id} contact={contact} />;
-          })}
+      <TransitionGroup>
+        {filtered === null
+          ? contacts.map(contact => {
+              return (
+                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              );
+            })
+          : filtered.map(contact => {
+              return (
+                <CSSTransition key={contact.id} timeout={500} classNames="item">
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              );
+            })}
+      </TransitionGroup>
     </Fragment>
   );
 };
