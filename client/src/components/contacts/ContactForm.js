e9dc185d340a0146d3aadfea515e10e2e3720a4f
VisheshSingh <visheshsingh16@gmail.com>
2019-07-20 00:10:48 -0500

    input fields added and useState hook

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -9,9 +9,59 @@ const ContactForm = () => {
   });
 
   const { name, email, phone, type } = contact;
+
+  const onChange = e => {
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
+
   return (
     <form>
       <h2 className="text-primary">Add Contact</h2>
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        placeholder="Phone"
+        name="phone"
+        value={phone}
+        onChange={onChange}
+      />
+      <h5>Add Type</h5>
+      <input
+        type="radio"
+        value="personal"
+        name="type"
+        checked={type === 'personal'}
+        onChange={onChange}
+      />{' '}
+      Personal{' '}
+      <input
+        type="radio"
+        value="professional"
+        name="type"
+        checked={type === 'professional'}
+        onChange={onChange}
+      />{' '}
+      Professional
+      <div>
+        <input
+          type="submit"
+          value="Add Contact"
+          className="btn btn-primary btn-block"
+        />
+      </div>
     </form>
   );
 };
