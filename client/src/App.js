6908e43572dc5d29dfc116355984e59b1ecf9386
VisheshSingh <visheshsingh16@gmail.com>
2019-07-21 13:15:30 -0500

    New routes added

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,33 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 function App() {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <div className="App">
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
+        <AlertState>
+          <Router>
+            <div className="App">
+              <Navbar />
+              <div className="container">
+                <Alert />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
             </div>
-          </div>
-        </Router>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
